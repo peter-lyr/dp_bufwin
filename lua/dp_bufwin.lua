81511d479f135419ee318e26fa06abd4684145f4
peter-lyr <llydrp.ldp@gmail.com>
2024-04-07 11:42:45 +0800

    function M.setup(options)

--- lua/dp_bufwin.lua
+++ lua/dp_bufwin.lua
@@ -4,4 +4,22 @@ function M.win_equal()
   vim.cmd 'wincmd ='
 end
 
+function M.win_max_height()
+  vim.cmd 'wincmd _'
+end
+
+M.defaults = {
+  ['<leader>'] = {
+    w = {
+      name = 'winbuf',
+      e = { M.win_equal, 'win equal', mode = { 'n', 'v', }, },
+      m = { M.win_max_height, 'win max height', mode = { 'n', 'v', }, },
+    },
+  },
+}
+
+function M.setup(options)
+  require 'which-key'.register(vim.tbl_deep_extend('force', {}, M.defaults, options or {}))
+end
+
 return M
